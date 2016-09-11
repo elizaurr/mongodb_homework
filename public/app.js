508a8a00e56819b7e63d9a5649593790ba0e22a1
elizaurr <elizaurreyes@yahoo.com>
2016-09-11 11:03:47 -0400

    redid site

---
+++ public/app.js
@@ -0,0 +1,107 @@
+//First scrap articles and return them to browser for rendering here.
+
+  $.getJSON('/scrape', function(data) {
+    console.log("data=",data);
+    for (var i = 0; i<data.length; i++){
+      //if there is a note on this article, attach *note* to the title to signify its existence to the user.
+      if(data[i].note){
+        noteExists = '<span style="color: yellow"><sup> *note attached*</sup></span>'
+        }else{
+          noteExists = "";
+      }
+      $('#articles').append('<h2><p data-id="' + data[i]._id + '">' + data[i].title + noteExists+'</p></h2>'+'<a href='+data[i].link +' target = "_blank">'+data[i].link+"<br />");
+      $('#articles').append("____________________________________________________________________________________________________________________");
+    }
+      $('#articles').append("____________________________________________________________________________________________________________________");
+  });
+
+//call scape function
+
+//When the article is clicked, the note input (or display) box is displayed with
+//either a savenote or deletenote button as appropriate.
+$(document).on('click', 'p', function(){
+  $('#notes').empty();
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    .done(function( data ) {
+      //console.log("app.js data=",data);
+      $('#notes').append('<h2>' + data.title + '</h2>');
+      $('#notes').append('<input id="titleinput" name="title" >');
+      $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
+      
+      //lets see if there is a note for this article and display it with delete btn
+      //or if not allow for entry of a note and save with save btn. 
+      //Color of input box changes depending on existence of note or not. 
+      if(data.note){
+        
+        $('#titleinput').val(data.note.title);
+        $('#bodyinput').val(data.note.body);
+        $('#notes').append('<button data-id="' + data._id + '" id="deletenote">Delete Note</button>');
+        $('#bodyinput,#titleinput').css('background-color', '#99ddff');
+      }else{
+        $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+        $('#bodyinput,#titleinput').css('background-color', '#99ddff');
+      }
+    });
+});
+//When the save note button is clicked POST to savenote on server
+$(document).on('click', '#savenote', function(){
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "POST",
+    url: "/savenote/" + thisId,
+    data: {
+      title: $('#titleinput').val(),
+      body: $('#bodyinput').val()
+    }
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').empty();
+    });
+
+    location.reload();
+  $('#titleinput').val("");
+  $('#bodyinput').val("");
+  
+});
+//When the delete note button is clicked Post to deletenote on server
+$(document).on('click', '#deletenote', function(){
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "POST",
+    url: "/deletenote/" + thisId,
+  })
+    .done(function( data ) {
+      
+      console.log(data);
+      $('#notes').empty();
+    });
+
+  location.reload();
+  $('#titleinput').val("");
+  $('#bodyinput').val("");
+  
+});
+
+$(document).on('click', '#cnnlogo', function(){
+$('#head').css("color", "white");
+  $.ajax({
+    method: "POST",
+    url: "/dropdb/"
+  })
+    .done(function( data ) {
+      console.log("back from drop");
+      $('#head').css("color", "black");
+      //this will force a page refresh which will force a GET to '/' and that will force
+      //the /scrape because it is the first thing that the JS does upon starting up.  ;-)
+      location.reload();
+    });
+
+});
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//dependencies
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,90 +6,166 @@ var mongoose = require('mongoose');
 var request = require('request');
 var cheerio = require('cheerio');
 
-
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
-
-app.use(express.static('pulic'));
+app.use(express.static('public'));
 
 
 var database = {
-    local: 'mongod://localhost/mongo_scaping_HW',
-    remote: 'mongodb://heroku_shsppdng:gtfhicbv58fjgahofnc18niebt@ds029456.mlab.com:29456/heroku_shsppdng'
+  
+  local : 'mongodb://localhost/mongo_scraping_HW',
+  remote : 'mongodb://heroku_shsppdng:gtfhicbv58fjgahofnc18niebt@ds029456.mlab.com:29456/heroku_shsppdng'
 }
+//This line is for cutting/pasting either local or remote for the choice of database.
+//  local during testing/remote at deployment
+var whichDb = database.remote;
+mongoose.connect(whichDb);
 
-//this connects to the remote mongoose db
-var remoteDb = database.remote;
-mongoose.connect(remoteDb);
+ db = mongoose.connection;
 
-db = mongoose.connection;
-
-//this console an error if no db found
-db.on('error', function(err) {
-    console.log('Mongoose Error', err)
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
 });
-
-//this console a successful connection was made
-db.once('open', function() {
-    console.log('Db connection', remoteDb)
+db.once('open', function () {
+  console.log('DB connection: ',whichDb);
 });
-
-//local port and deployement port
-var PORT = process.env.PORT || 3309;
-//schemas
+var PORT = process.env.PORT || 8080; // Sets an initial port. We'll use this later in our listener
+//Require Schemas
 var Note = require('./models/Note.js');
 var Article = require('./models/Article.js');
 
+// Routes
 
-//routes
+//scrape articles, place in MongoDB, and return them in doc for rendering in browser
+app.get('/scrape', function(req, res){
+    request('http://www.espn.com/', function(error, response, html) {
+    var $ = cheerio.load(html);
+    
+        $('articles.item-info-wrap').each(function(i, element) {
+          var result = {};
+          result.title = $(this).find('span').text();
+          result.link = $(this).children('a').attr('href');
+          //some CNN links already have http to an outside site otherwise they are cnn links.
+          if(result.link.indexOf("http")<0){
+            result.link='http://www.espn.com'+result.link;
+          }
 
-app.get('/', function(req, res) {
-    res.send(index.html);
+          var entry = new Article (result);
+          entry.save(function(err, doc) {
+            if (err) {
+              console.log(err);
+            } 
+          })
+      })
+      
+      Article.find({}, function(err, doc){
+        if (err){
+          console.log(err);
+        } else {
+          res.json(doc);
+        }
+      })
+  })
 });
 
-//a get request to scrape from espn website
-app.get('/scrape', function(req, res) {
-    request('http://www.espn.com/', function(error, response, html) {
+//this gets the article and note and returns as a JSON to be used when
+//displaying the text box for the note.  (headline goes above text box)
+app.get('/articles/:id', function(req, res){
+  Article.findOne({'_id': req.params.id})
+  .populate('note')
+  .exec(function(err, doc){
+    if (err){
+      console.log(err);
+    } else {
+      res.json(doc);
+    }
+  });
+});
 
-        //then we load it into cheerio and save it into $ for short-hand selector
-        var $ = cheerio.load(html);
+//adds the note id to the article document as a reference back to the note
+app.post('/savenote/:id', function(req, res){
+  var newNote = new Note(req.body);
 
-        //now we grab all the articles
-        $('articles .item-info-wrap').each(function(i, element) {
+  newNote.save(function(err, doc){
+    if(err){
+      console.log(err);
+    } else {
+      Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
+      .exec(function(err, doc){
+        if (err){
+          console.log(err);
+        } else {
+          res.send(doc);
+        }
+      });
 
-            //save an empty result 
-            var result = {};
+    }
+  });
+});
 
-            //add the text and href of every link
-            //and save them as properties of the result
-            result.title = $(this).children('a').text();
-            result.link = $(this).children('a').attr('href');
+//delete the note from both collections (article and notes)
+app.post('/deletenote/:id', function(req, res){
+      Article.find({'_id': req.params.id}, 'note', function(err,doc){
+      // .exec(function(err, doc){
+        if (err){
+          console.log(err);
+        }
+        //deletes the note from the Notes Collection
+          Note.find({'_id' : doc[0].note}).remove().exec(function(err,doc){
+            if (err){
+              console.log(err);
+            }
 
-            // using our Article model, create a new entry.
-            // Notice the (result):
-            // This effectively passes the result object to the entry (and the title and link)
-            var entry = new Article(result);
+          });
+        
+      });
+      //deletes the note reference in the article document
+      Article.findOneAndUpdate({'_id': req.params.id}, {$unset : {'note':1}})
+      .exec(function(err, doc){
+        if (err){
+          console.log(err);
+        } else {
+          res.send(doc);
+        }
+      });
+});
 
-            //now save that entry to the db
-            entry.save(function(err, doc) {
-
-                //log any error
-                if (err) {
-                    console.log(err);
-                }
-                // or log the doc
-                else {
-                    console.log(doc)
-                }
-
-            });
-        });
-
-
-    });
-
-    // tell the brower that we finshed scraping 
-    res.send("Scrape Complete");
+app.post('/dropdb', function(req,res){
+//this function will delete all articles except those that have user notes.
+//once it goes back to the client, the page will be refreshed which forces
+//a new GET for the latest articles on the CNN Top Stores area of their home page.
+  Article.find({})
+    .populate('note')
+    .exec(function(err, doc){
+        if (err){
+          console.log(err);
+        } else {
+              var removedArticles = 0;
+             for(i=0;i<doc.length;i++){
+              // console.log(doc[i]._id);
+              // console.log(doc[i].note);
+              //if there is no note, we can remove the article from the db
+              //but if there is a note, move on to the next article.
+                    
+                    if(doc[i].note==undefined){
+                        Article.find({'_id' : doc[i]._id}).remove()
+                        .exec(function(err, doc){
+                            if (err){
+                              console.log(err);
+                            }else{
+                              ++removedArticles;
+                              console.log(removedArticles+" Total Articles removed");
+                            }//close else
+                        })//close .exec
+                    }//close if
+            }//close for
+        }//close else 
+    })//close .exec
+res.end();
+});//close drop route
+//start express server
+app.listen(PORT, function() {
+    console.log("Server listening on PORT: " + PORT);
 });
